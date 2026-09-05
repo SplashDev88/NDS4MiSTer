098ad106ac27001d045d71a4aefbb7f5b833dcf2
SplashDev88 <321907889+SplashDev88@users.noreply.github.com>
2026-09-05 15:52:02 -0500

    build: identify touch sound FreeBIOS seed-2 candidate

--- fpga/mister_nitro_console_island/sys/build_id.tcl
+++ fpga/mister_nitro_console_island/sys/build_id.tcl
@@ -5,7 +5,7 @@
 proc generateBuildID_Verilog {} {
 
 	# Get the timestamp (see: http://www.altera.com/support/examples/tcl/tcl-date-time-stamp.html)
-	set buildDate "`define BUILD_DATE \"260903-B7RC134\""
+	set buildDate "`define BUILD_DATE \"260905-TSAUDS2\""
 
 	# Create a Verilog file for output
 	set outputFileName "build_id.v"
